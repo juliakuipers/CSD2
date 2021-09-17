a134ca2e8267d96c0b6e8a697511fcb599317db2
julia kuipers <juliakuipers64@gmail.com>
2021-09-17 12:17:56 +0200

    added duration per note

--- CSD2a/python_basics/ritmische_playback.py
+++ CSD2a/python_basics/ritmische_playback.py
@@ -1,16 +1,27 @@
 import simpleaudio as sa 
+import asyncio
 
 Kick = "Kick.wav"
 wave_obj = sa.WaveObject.from_wave_file(Kick)
 
 numPlaybackTimes = int(input("hoe vaak wil je de kick afspelen?"))
-dur = int(input("noem de duratie van elke kick")) 
-bpm = int(input("wat is het BPM?"))
+print("noem de duratie van elke kick")
+dur = []
 
-i =1  
+while numPlaybackTimes > dur: 
+    dur = int(input(""))
+
+bpm = int(input("wat is het BPM?"))
+wait = 60/bpm 
+
+async def duration():
+    await asyncio.sleep(dur)
+
+
+i =1 
 for i in range(numPlaybackTimes):  
     play_obj = wave_obj.play()
     play_obj.wait_done()
-    #wacht een bepaalde tijd van het BPM 
+    asyncio.run(duration()) 
  
 
