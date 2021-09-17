90f0fdc907980af576aefe9b3b858d1178374e01
julia kuipers <juliakuipers64@gmail.com>
2021-09-17 17:01:30 +0200

    made some progress with duration per note

--- CSD2a/python_basics/ritmische_playback.py
+++ CSD2a/python_basics/ritmische_playback.py
@@ -5,21 +5,27 @@ Kick = "Kick.wav"
 wave_obj = sa.WaveObject.from_wave_file(Kick)
 
 numPlaybackTimes = int(input("hoe vaak wil je de kick afspelen?"))
-print("noem de duratie van elke kick")
 dur = []
+print("noem %d duraties van elke kick"% numPlaybackTimes)
+# %d roept de integer van numPlaybackTimes als ik dat specificeer aan het einde van de regel met % 
 
-while numPlaybackTimes > dur: 
-    dur = int(input(""))
+i=0
+for i in range(numPlaybackTimes): 
+    durar = float(input(""))
+    dur.append(durar)
+    
+
+async def duration():
+    await asyncio.sleep([dur])
+    #plaatsing van deze code moet hoger?
+
+print(dur)
 
 bpm = int(input("wat is het BPM?"))
 wait = 60/bpm 
 
-async def duration():
-    await asyncio.sleep(dur)
-
-
-i =1 
-for i in range(numPlaybackTimes):  
+a =1 
+for a in range(numPlaybackTimes):  
     play_obj = wave_obj.play()
     play_obj.wait_done()
     asyncio.run(duration()) 
