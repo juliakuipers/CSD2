246b421378b4a105141308cc5cf5664f8353b348
julia kuipers <juliakuipers64@gmail.com>
2021-09-17 10:51:17 +0200

    made changes in code

--- CSD2a/python_basics/playsound.py
+++ CSD2a/python_basics/playsound.py
@@ -2,7 +2,14 @@ import simpleaudio as sa
 
 filename = 'loop.wav'
 wave_obj = sa.WaveObject.from_wave_file(filename)
+
+
 play_obj = wave_obj.play()
-play_obj.wait_done()
+playstop = input("")
+ophouden = "stop"
+if playstop == ophouden:
+    play_obj.stop
+   
+    
 
 
