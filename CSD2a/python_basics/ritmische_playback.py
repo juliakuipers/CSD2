94ab11c97bbf0556bcf285dd11175640e06fa8c8
julia kuipers <juliakuipers64@gmail.com>
2021-09-20 01:04:32 +0200

    made changes in code

--- CSD2a/python_basics/ritmische_playback.py
+++ CSD2a/python_basics/ritmische_playback.py
@@ -6,32 +6,32 @@ wave_obj = sa.WaveObject.from_wave_file(Kick)
 
 numPlaybackTimes = int(input("hoe vaak wil je de kick afspelen?"))
 # %d roept de integer van numPlaybackTimes als ik dat specificeer aan het einde van de regel met % 
-
-dur = []
 print("noem %d duraties van elke kick"% numPlaybackTimes)
-   
-i=0 
-#for i in range(numPlaybackTimes): 
- #   durar = float(input(""))
-#    dur.append(durar)
+dur = []
+    
+for i in range(numPlaybackTimes): 
+    durar = float(input(""))
+    dur.append(durar)
 
-async def duration():
-    for i in range(numPlaybackTimes): 
-        durar = float(input(""))
-        dur.append(durar)
-    await asyncio.sleep(durar)
-    #plaatsing van deze code moet hoger?
 
-# while loop met def dan de for loop +1 totdat de while loop niet meer geld 
 
 bpm = int(input("wat is het BPM?"))
-wait = 60/bpm 
+#sec = dur * bpm
+#wait = sec/bpm 
+#print(wait)
 
-a = 1 
-for a in range(numPlaybackTimes):  
+async def duration():
+    for i in range(numPlaybackTimes):
+        await asyncio.sleep(dur)
+# while loop met def dan de for loop +1 totdat de while loop niet meer geld 
+
+
+
+for i in range(numPlaybackTimes):  
     play_obj = wave_obj.play()
     play_obj.wait_done()
-    asyncio.run(duration()) 
+    asyncio.run(duration())
+
     #i+ 1
  
 
