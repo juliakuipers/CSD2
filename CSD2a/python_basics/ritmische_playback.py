8047bf57b5e137ccde3e89216b7bfe9d2c7cf3dd
julia kuipers <juliakuipers64@gmail.com>
2021-09-21 13:53:16 +0200

    fixed duration

--- CSD2a/python_basics/ritmische_playback.py
+++ CSD2a/python_basics/ritmische_playback.py
@@ -2,9 +2,6 @@ import simpleaudio as sa
 # as in als: sa om te verwijzen na simpleaudio als sa 
 import asyncio as ac
 
-Kick = "Kick.wav"
-wave_obj = sa.WaveObject.from_wave_file(Kick)
-
 numPlaybackTimes = int(input("hoe vaak wil je de kick afspelen?"))
 # %d roept de integer van numPlaybackTimes als ik dat specificeer aan het einde van de regel met % 
 print("noem %d duraties van elke kick"% numPlaybackTimes)
@@ -19,13 +16,14 @@ for i in range(numPlaybackTimes):
 bpm = int(input("wat is het BPM?"))
 
 async def duration():
+    i = 0
     for i in range(numPlaybackTimes):
     #de for loop runt het aantal keren numPlaybackTimes 
-        #for i in range(dur[i]):
-        #andere loop 
+        Kick = "Kick.wav"
+        wave_obj = sa.WaveObject.from_wave_file(Kick)
         wave_obj.play()
-        await ac.sleep(bpm / dur[i])
-
+        await ac.sleep(dur[i])
+        i +1
 ac.run(duration())
 #roept de def aan met de sequencer 
 
