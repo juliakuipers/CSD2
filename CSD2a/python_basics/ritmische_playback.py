5738f9b04b4d14cfa00c02d47b8f66c8c776f529
julia kuipers <juliakuipers64@gmail.com>
2021-09-24 10:43:31 +0200

    worked on assigment 3

--- CSD2a/python_basics/ritmische_playback.py
+++ CSD2a/python_basics/ritmische_playback.py
@@ -1,6 +1,7 @@
 import simpleaudio as sa 
 # as in als: sa om te verwijzen na simpleaudio als sa 
 import asyncio as ac
+import time as tm 
 
 numPlaybackTimes = int(input("hoe vaak wil je de kick afspelen?\n"))
 # %d roept de integer van numPlaybackTimes als ik dat specificeer aan het einde van de regel met % 
@@ -15,15 +16,33 @@ for i in range(numPlaybackTimes):
 
 bpm = int(input("wat is het BPM?\n"))
 
+#counter = tm.time() - tm.time()
+#for i in range(numPlaybackTimes):
+    #print("counter=",counter)
+    #tm.sleep(notedur[i])
+
+notedur = []
+for i in range(numPlaybackTimes):
+    notedur.append((dur[i]* 60) / bpm)
+    print("dur=",dur[i])
+
+print("notedur:",notedur)
+#for i in range (numPlaybackTimes):
+
+counter = []  
 async def duration():
     i = 0
     for i in range(numPlaybackTimes):
     #de for loop runt het aantal keren numPlaybackTimes 
+        count = notedur[i]
+        #geen for loop gebruiken, de loop moet de hele tijd afwachtend zijn op wanneer de volgende noot gespeeld moet worden
         Kick = "Kick.wav"
         wave_obj = sa.WaveObject.from_wave_file(Kick)
         wave_obj.play()
-        await ac.sleep((dur[i]*60)/bpm)
+        await ac.sleep(notedur[i])
         i +1
+
+#als iets gelijk is aan de counter speel dan af, dit kan met tijd die telt vanaf 0 en een array die de timestamps heeftr 
 ac.run(duration())
 #roept de def aan met de sequencer 
 
