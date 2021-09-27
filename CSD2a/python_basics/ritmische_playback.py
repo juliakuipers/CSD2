723818777f3fe92747283ff32fd7a1828333baee
julia kuipers <juliakuipers64@gmail.com>
2021-09-27 16:25:18 +0200

    made 16th grid work

--- CSD2a/python_basics/ritmische_playback.py
+++ CSD2a/python_basics/ritmische_playback.py
@@ -4,9 +4,9 @@ import asyncio as ac
 import time as tm 
 
 numPlaybackTimes = int(input("hoe vaak wil je de kick afspelen?\n"))
-# %d roept de integer van numPlaybackTimes als ik dat specificeer aan het einde van de regel met % 
+    # %d roept de integer van numPlaybackTimes als ik dat specificeer aan het einde van de regel met % 
 print("noem %d duraties van elke kick"% numPlaybackTimes)
-#kan ook met list() 
+    #kan ook met list() 
 
 dur = []    
 for i in range(numPlaybackTimes): 
@@ -14,7 +14,14 @@ for i in range(numPlaybackTimes):
     dur.append(duration_array)
     #zorgt ervoor dat je het aantal numplaybacktimes in een float stopt die het in een array stopt; dur 
 
-bpm = int(input("wat is het BPM?\n"))
+changeBPM = input("Het BPM is 120, wil je dit veranderen? (typ ja/nee)\n")
+
+if changeBPM == "ja":
+    bpm = int(input("voer het gewilde bpm in\n"))
+elif changeBPM == "nee":
+    bpm = 120
+else: 
+    "voer opnieuw in"
 
 notedur = []
 for i in range(numPlaybackTimes):
@@ -22,22 +29,19 @@ for i in range(numPlaybackTimes):
     print("dur=",dur[i])
 
 print("notedur:",notedur)
-#for i in range (numPlaybackTimes):
 
-counter = []
+timestamps16th = []
 currentcount = 0.0
-#counter = tm.time() - tm.time()
+
 for i in range(numPlaybackTimes):
     print(currentcount)
-    currentcount += 1
-    print("currrentcount=",currentcount)
-    print("counter=",counter)
-    #tm.sleep(notedur[i])
-    print("currentcount=",currentcount,"notedur[i]=",notedur[i])
-    currentcount =+ notedur[i]
-    #print("cur=",cur)
+    timestamps16th.append(currentcount/0.25)
+    #deelt zodat het in 16e past 
+    currentcount = currentcount + notedur[i]
+    print("t",timestamps16th)
+#print(currentcount)
 
-  
+#function gebruiken
 async def duration():
     i = 0   
     for i in range(numPlaybackTimes):
@@ -47,10 +51,8 @@ async def duration():
         wave_obj = sa.WaveObject.from_wave_file(Kick)
         wave_obj.play()
         await ac.sleep(notedur[i])
-
         #deze moet dus in een grid van 16e passen 
         
-
 #als iets gelijk is aan de counter speel dan af, dit kan met tijd die telt vanaf 0 en een array die de timestamps heeftr 
 ac.run(duration())
 #roept de def aan met de sequencer x
