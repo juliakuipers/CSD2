2a87fcc9338dd77de12766c76bf53224132f1e2c
julia kuipers <juliakuipers64@gmail.com>
2022-02-09 18:56:37 +0100

    i think i added tremolo

--- CSD2c/1.les/1.opdracht/main.cpp
+++ CSD2c/1.les/1.opdracht/main.cpp
@@ -32,7 +32,7 @@ int main(int argc,char **argv)
   jack.init(argv[0]);
   double samplerate = jack.getSamplerate();
   Sine osc(440, samplerate);
-  Tremolo tremolo(440,samplerate);
+  Tremolo tremolo(220,samplerate);
   // CircularBuffer cb(samplerate*5,samplerate);
 
 
@@ -40,7 +40,7 @@ int main(int argc,char **argv)
   WriteToFile fileWriter("output.csv", true);
 
   for(int i = 0; i < 500; i++) {
-    fileWriter.write(std::to_string(osc.getSample()) + "\n");
+    fileWriter.write(std::to_string(tremolo.process(osc.getSample())) + "\n");
     // cb.write(osc.getSample());
     osc.genNextSample();
     // cb.read();
