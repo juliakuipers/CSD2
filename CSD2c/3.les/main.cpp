1b9d7a9bcac6229f0a44e6a0a83150e56a9e4c31
julia kuipers <juliakuipers64@gmail.com>
2022-02-24 18:03:26 +0100

    tested stereo and it works

--- CSD2c/3.les/main.cpp
+++ CSD2c/3.les/main.cpp
@@ -2,28 +2,28 @@
 #include <thread>
 #include <unistd.h>
 #include "jack_moduleStereo.h"
-#include "oscillator.h"
+#include "square.h"
 
 JackModule jack;
 unsigned long samplerate=44100;
 unsigned long chunksize = 256;
-Oscillator* osc = nullptr;
-Oscillator* osc = nullptr;
 
 bool running = true;
 static void audioProcess()
 {
   float *inBuffer = new float [chunksize];
   float *outBuffer = new float [chunksize*2];
+  Square osc1(440,samplerate);
+  Square osc2(220,samplerate);
+  float amp = 0.2;
   do{
       jack.readSamples(inBuffer,chunksize);
       for(unsigned int i = 0 ; i<chunksize ; i++)
       {
         outBuffer[2*i] = osc1.getSample()*amp;
         outBuffer[2*i+1] = osc2.getSample()*amp;
-        osc1.genNextSample()
-        osc2.genNextSample()
-        std::cout << "audioProcess \n";
+        osc1.genNextSample();
+        osc2.genNextSample();
         }
       jack.writeSamples(outBuffer,chunksize*2);
   }while(running);
@@ -32,14 +32,11 @@ static void audioProcess()
 
 int main(int argc,char **argv)
 {
-  jack.setNumberOfInputChannels(1);
+  jack.setNumberOfInputChannels(2);
   jack.setNumberOfOutputChannels(2);
   jack.init(argv[0]);
   jack.autoConnect();
   samplerate=jack.getSamplerate();
-  osc1 = new Sine(440,samplerate);
-  osc2 = new Sine(220,samplerate);
-  float amp = 0.2;
   std::cout << "samplerate = " << samplerate << std::endl;
 
   std::cout << "running \n";
@@ -54,10 +51,6 @@ int main(int argc,char **argv)
     }
   }
   audioThread.join();
-  delete osc1;
-  delete osc2;
-  osc1 nullptr;
-  osc2 nullptr;
   //ending the thread
   return 0;
 }
